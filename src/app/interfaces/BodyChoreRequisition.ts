655e395aab57a40287fa481487401c046a889252
Leonardo Bloise <leonardo.bloise2000@gmail.com>
2020-08-25 15:17:11 -0300

    Organizando interfaces

---
+++ src/app/interfaces/BodyChoreRequisition.ts
@@ -0,0 +1,10 @@
+interface BodyChoreRequisition extends Body {
+    title: string
+    activity?: string
+    timefrom: string
+    timeto: string
+    description?: string
+}
+
+
+export default BodyChoreRequisition
